a51bb231fc083e6f17b1ee06f768093065f8deff
NanaKwameCheddar <126354466+NanaKwameCheddar@users.noreply.github.com>
2024-12-27 17:59:42 +0000

    hold on cards

--- src/components/admin/Analytics.tsx
+++ src/components/admin/Analytics.tsx
@@ -104,7 +104,7 @@ export function Analytics() {
         </Select>
       </div>
 
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+{/*       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Total Parcels</CardTitle>
@@ -231,7 +231,7 @@ export function Analytics() {
             </ChartContainer>
           </CardContent>
         </Card>
-      </div>
+      </div> */}
     </div>
   )
 }
